04666f29d5bd8cdd038f770cf09617afbd72201b
logan764 <72811627+logan764@users.noreply.github.com>
2024-02-23 10:14:47 -0500

    Update far_60053a.cpp

--- far_60053a.cpp
+++ far_60053a.cpp
@@ -1,4 +1,4 @@
-//WHEN UPLOADING TO V5, MAKE SURE THIS FITS NEARSIDE OR FARSIDE
+///WHEN UPLOADING TO V5, MAKE SURE THIS FITS NEARSIDE OR FARSIDE
 //EDIT VAR BELOW
 // 1 = FARSIDE 
 //-1 = NEARSIDE
@@ -11,7 +11,7 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
-#include <list>
+
 
 #include "vex.h"
 
@@ -33,31 +33,9 @@ brain Brain;
 
 
 // Robot configuration code.
-controller Controller1 = controller(primary);
-digital_out wings = digital_out(Brain.ThreeWirePort.H);
-bool holdit = true;
-motor mR1 = motor(PORT3, ratio18_1, false);
 
-motor mArmR = motor(PORT4, ratio18_1, true);
 
-motor mR3 = motor(PORT6, ratio18_1, true);
 
-motor mL3 = motor(PORT8, ratio18_1, false);
-
-motor mL1 = motor(PORT9, ratio18_1, true);
-
-motor mR2 = motor(PORT14, ratio18_1, false);
-
-motor mL2 = motor(PORT18, ratio18_1, true);
-
-motor mArmL = motor(PORT19, ratio18_1, false);
-
-motor_group arm= motor_group(mArmL, mArmR);
-motor_group mLeft = motor_group(mL1, mL2, mL3);
-motor_group mRight = motor_group(mR1, mR2, mR3);
-//inertial imu = inertial()
-//smartdrive Drivetrain = smartdrive(mLeft, mRight, imu, 319.19, 255, 235, mm, .5*0.5714285714285714);
-drivetrain Drivetrain = drivetrain(mLeft, mRight, 319.19, 255, 235, mm, .5*0.5714285714285714);
 
 // Helper to make playing sounds from the V5 in VEXcode easier and
 // keeps the code cleaner by making it clear what is happening.
@@ -66,72 +44,34 @@ void playVexcodeSound(const char *soundName) {
   wait(5, msec);
 }
 
-// define variable for remote controller enable/disable
-bool RemoteControlCodeEnabled = true;
-// define variables used for controlling motors based on controller inputs
-bool DrivetrainLNeedsToBeStopped_Controller1 = true;
-bool DrivetrainRNeedsToBeStopped_Controller1 = true;
-
-// define a task that will handle monitoring inputs from Controller1
-int rc_auto_loop_function_Controller1() {
-  // process the controller input every 20 milliseconds
-  // update the motors based on the input values
-  while(true) {
-    if(RemoteControlCodeEnabled) {
-      
-      // calculate the drivetrain motor velocities from the controller joystick axies
-      // left = Axis3 + Axis1
-      // right = Axis3 - Axis1
-      int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
-      int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
-      
-      // check if the value is inside of the deadband range
-      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
-        // check if the left motor has already been stopped
-        if (DrivetrainLNeedsToBeStopped_Controller1) {
-          // stop the left drive motor
-          mLeft.stop();
-          // tell the code that the left motor has been stopped
-          DrivetrainLNeedsToBeStopped_Controller1 = false;
-        }
-      } else {
-        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
-        DrivetrainLNeedsToBeStopped_Controller1 = true;
-      }
-      // check if the value is inside of the deadband range
-      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
-        // check if the right motor has already been stopped
-        if (DrivetrainRNeedsToBeStopped_Controller1) {
-          // stop the right drive motor
-          mRight.stop();
-          // tell the code that the right motor has been stopped
-          DrivetrainRNeedsToBeStopped_Controller1 = false;
-        }
-      } else {
-        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
-        DrivetrainRNeedsToBeStopped_Controller1 = true;
-      }
-      
-      // only tell the left drive motor to spin if the values are not in the deadband range
-      if (DrivetrainLNeedsToBeStopped_Controller1) {
-        mLeft.setVelocity(drivetrainLeftSideSpeed, percent);
-        mLeft.spin(forward);
-      }
-      // only tell the right drive motor to spin if the values are not in the deadband range
-      if (DrivetrainRNeedsToBeStopped_Controller1) {
-        mRight.setVelocity(drivetrainRightSideSpeed, percent);
-        mRight.spin(forward);
-      }
-    }
-    // wait before repeating the process
-    wait(20, msec);
-  }
-  return 0;
-}
-
-task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
-
 #pragma endregion VEXcode Generated Robot Configuration
+controller Controller1 = controller(primary);
+digital_out wings = digital_out(Brain.ThreeWirePort.H);
+bool holdit = true;
+motor mR1 = motor(PORT3, ratio6_1, false);
+
+motor mArmR = motor(PORT4, ratio18_1, true);
+
+motor mR3 = motor(PORT6, ratio6_1, true);
+
+motor mL3 = motor(PORT8, ratio6_1, false);
+
+
+
+motor mL1 = motor(PORT9, ratio6_1, true);
+
+motor mR2 = motor(PORT14, ratio6_1, false);
+
+motor mL2 = motor(PORT18, ratio6_1, true);
+
+motor mArmL = motor(PORT19, ratio18_1, false);
+
+motor_group arm= motor_group(mArmL, mArmR);
+motor_group mLeft = motor_group(mL1, mL2, mL3);
+motor_group mRight = motor_group(mR1, mR2, mR3);
+inertial imu = inertial(PORT11);
+smartdrive Drivetrain = smartdrive(mLeft, mRight, imu, 319.19, 255, 235, mm, .5*0.5714285714285714);
+//drivetrain Drivetrain = drivetrain(mLeft, mRight, 319.19, 255, 235, mm, .5*0.5714285714285714);
 
 
 // ----------------------------------------------------------------------------
@@ -195,11 +135,16 @@ void autonomous(void) {
   Brain.Screen.clearScreen();
   Brain.Screen.print("autonomous code");
   arm.setVelocity(5400,rpm);
-  _90fwd();
-  _90rvs();
-  Drivetrain.setDriveVelocity(50, percent);
-  Drivetrain.setTurnVelocity(50, percent);
-  Drivetrain.driveFor(forward,12+3.24803,inches);
+  arm.setStopping(hold);
+  arm.stop();
+  //_90fwd();
+  //_90rvs();
+  Drivetrain.setDriveVelocity(40, percent);
+  Drivetrain.setTurnVelocity(40, percent);
+  Drivetrain.driveFor(forward,10+3.24803,inches);
+  Drivetrain.stop();
+  wait(15,seconds);\
+
   Drivetrain.turnFor(right, invert*45, degrees);
   Drivetrain.driveFor(forward,22.869756222+3.24803,inches);
   Drivetrain.turnFor(left,invert*45,degrees);
@@ -262,13 +207,15 @@ double canPress = true;
     else if(Controller1.ButtonL2.pressing()){_90rvs();}
     else if(Controller1.ButtonR1.pressing()){arm.spin(forward,12,volt);}//R1 and R2 yeet it like the old program
     else if(Controller1.ButtonR2.pressing()){arm.spin(forward,-12,volt);}
-    else if(Controller1.ButtonL2.pressing() && Controller1.ButtonL1.pressing() && canPress==true){
-    stop_hammer_time();//swap between coast and hold stopping patterns
-    arm.stop();
-    canPress=false;
+    else if (Controller1.ButtonL2.pressing()){
+      arm.setPosition(fmod(arm.position(degrees),360),degrees);
+      arm.spinToPosition(45,degrees,false);
     }
+    //stop_hammer_time();//swap between coast and hold stopping patterns
+    //arm.stop();
+    //canPress=false;
+  
     else{arm.stop(); canPress=true;}
-    Controller1.ButtonA.pressed(turnAround);
     mLeft.spin(forward,Controller1.Axis3.position()*.12+Controller1.Axis1.position()*.75*.12, volt);
     mRight.spin(forward,Controller1.Axis3.position()*.12-Controller1.Axis1.position()*.75*.12, volt);
     //one-stick drive
@@ -295,4 +242,5 @@ int main() {
 
   // Prevent main from exiting with an infinite loop.
   
+}
 }
